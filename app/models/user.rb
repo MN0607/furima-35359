b2df906e7a2c33bff71687751f6be15106be4748
MN0607 <kobukuro.mun@gmail.com>
2021-07-02 13:45:20 +0900

    新規登録のバリデーション、エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :first_name,          presence: true
-  validates :last_name,           presence: true
-  validates :first_name_hurigana, presence: true
-  validates :last_name_hurigana,  presence: true
-  validates :birthday,            presence: true
-  
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :last_name,  format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :first_name_hurigana, format: {with: /\A[ァ-ンー]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :last_name_hurigana,  format: {with: /\A[ァ-ンー]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :birthday
+  end
+
+  validates :encrypted_password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+
 end
