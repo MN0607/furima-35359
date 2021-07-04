27df58c6a255be4ff99dcd645843f0231c11211e
MN0607 <kobukuro.mun@gmail.com>
2021-07-04 15:22:25 +0900

    【修正２】コメントアウト削除、バリデーション/テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-  end
 
-  with_options presence: true , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } do
-    validates :first_name
-    validates :last_name
-  end
-  
-  with_options presence: true , format: {with: /\A[ァ-ンー]+\z/, message: "is invalid. Input full-width katakana characters" } do
-    validates :first_name_hurigana
-    validates :last_name_hurigana
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" } do
+      validates :first_name
+      validates :last_name
+    end
+    
+    with_options format: {with: /\A[ァ-ンー]+\z/, message: "is invalid. Input full-width katakana characters" } do
+      validates :first_name_hurigana
+      validates :last_name_hurigana
+    end
   end
 
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
