75962a33f4c14bb472dca3f65e9b24f42ac51c40
MN0607 <kobukuro.mun@gmail.com>
2021-07-08 09:34:30 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    # AS導入に伴い記述。merge等追加必要。
+    params.require(:item).permit(:image)
+  end
 end
