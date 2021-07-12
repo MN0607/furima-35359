94f34e24985eb2b36e8172400283ecc3715b9f8f
MN0607 <kobukuro.mun@gmail.com>
2021-07-12 18:02:10 +0900

    出品商品のテーブルへの保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    # AS導入に伴い記述。merge等追加必要。
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:trade_name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
