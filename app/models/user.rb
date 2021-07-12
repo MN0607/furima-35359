658adee00fd14993c3cd143d921ba8dee412662e
MN0607 <kobukuro.mun@gmail.com>
2021-07-12 18:52:38 +0900

    【修正１】

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,5 +22,5 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 
-  has_many :users
+  has_many :items
 end
