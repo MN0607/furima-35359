9d73d97d6f2171897e1c91fbd4cc11583bc8dc99
MN0607 <kobukuro.mun@gmail.com>
2021-07-16 22:22:52 +0900

    [修正1]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :get_item_params, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,8 +41,13 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def get_item_params
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
     @item = Item.find(params[:id])
     redirect_to root_path unless current_user == @item.user
   end
+
 end
