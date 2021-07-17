b7ccd2f9ecd5af28d6eff9fa2308f5946e6a8388
MN0607 <kobukuro.mun@gmail.com>
2021-07-17 11:04:47 +0900

    [修正1]

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :get_item_params, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
