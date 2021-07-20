fead92ab28ec3ff8f7ce487b25ea677297449eea
MN0607 <kobukuro.mun@gmail.com>
2021-07-20 22:07:52 +0900

    formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :token
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :user_id, :item_id, :municipality, :house_number
+    validates :phone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input only number"}
+  end
+
+  validates :phone_number, length: { minimum: 10, maximum: 11, message: "is too short"}
+
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
