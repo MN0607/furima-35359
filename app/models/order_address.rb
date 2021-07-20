69df0aac5b059078c8231abe75496ee60e24cdc1
MN0607 <kobukuro.mun@gmail.com>
2021-07-20 22:23:55 +0900

    テストコード実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,6 +3,7 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :user_id, :item_id, :municipality, :house_number
