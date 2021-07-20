705c51c3e99feb56337abf1cca964812daf837a4
MN0607 <kobukuro.mun@gmail.com>
2021-07-20 22:08:58 +0900

    orderコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      Payjp.api_key = "sk_test_2f51c89ae081b910cf5b99be" 
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+end
